9819335d1c11b0e743e4dd25c54f5a06a70b4ecb
“Cody <cbandrowski@gmail.com//>
2025-02-08 11:59:36 -0500

    listed loaded issue with remove

--- src/main/java/org/example/csc329_hw1/HelloController.java
+++ src/main/java/org/example/csc329_hw1/HelloController.java
@@ -1,16 +1,25 @@
 package org.example.csc329_hw1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.awt.event.ActionEvent;
 
 public class HelloController {
+    public ListView<String> uploadedList;
+    public ListView removedList;
+    public Label sizeLabel;
+    private PQList pqList = new PQList();
     @FXML
     private Label welcomeText;
+
 
     public void uploadFile(javafx.event.ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -18,6 +27,40 @@ public class HelloController {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text File", "*.txt"));
         File selectedFile = fileChooser.showOpenDialog(new Stage());
 
+
+        if(selectedFile == null) {
+            uploadedList.getItems().add("File is Empty");
+            return;
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(selectedFile))){
+            String nameLine;
+            while((nameLine = br.readLine()) != null){
+                String scoreLine = br.readLine();
+                int score = Integer.parseInt(scoreLine.trim());
+                Player player = new Player(nameLine, score);
+                pqList.add(player);
+            }
+        }catch (IOException | NumberFormatException e){
+            e.printStackTrace();
+        }
+        updateListView();
+    }
+    private void updateListView(){
+        if(pqList.isEmpty()){
+            uploadedList.getItems().clear();
+            uploadedList.getItems().add("No players found in list");
+            return;
+        }
+        uploadedList.getItems().clear();
+        String[] playerList = pqList.toString().split("\n");
+        uploadedList.getItems().addAll(playerList);
+        sizeLabel.setText(Integer.toString(uploadedList.getItems().size()));
     }
 
+    public void removeHighest(ActionEvent actionEvent) {
+       Player highest = pqList.getHighestScoreplayer();
+       removedList.getItems().add(highest.toString());
+       updateListView();
+    }
 }
--- src/main/java/org/example/csc329_hw1/PQList.java
+++ src/main/java/org/example/csc329_hw1/PQList.java
@@ -1,65 +1,121 @@
 package org.example.csc329_hw1;
 import org.example.csc329_hw1.Player;
 
-public class PQList implements PlayerADT{
+/**
+ * PQList- implements PlayerADT interface
+ * Class will manage the list of players taken in from files
+ * @author Cody Bandrowski
+ */
+public class PQList implements PlayerADT {
 
- private org.example.csc329_hw1.Node head;
- private org.example.csc329_hw1.Node tail;
- private int size;
+    private org.example.csc329_hw1.Node head;
+    private org.example.csc329_hw1.Node tail;
+    private int size;
 
     /**
      * Default Constructor
-     *
      */
-    public PQList(){
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-}
+    public PQList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
     /**
      * Copy Constructor
      *
      * @param other
      */
-    public PQList(PQList other){
+    public PQList(PQList other) {
         this.head = other.head;
         this.tail = other.tail;
         this.size = other.size;
 
-}
+    }
 
-public PQList createClone(){
+    /**
+     * createClone
+     * Method makes a clone of another PQList
+     * @return
+     */
+    public PQList createClone() {
         PQList clone = new PQList(); //Makes Empty PQlist
         Node temp = this.head; //Makes temp the head of Original PQlist
-        while(temp != null){ //This will go until tail.next if reached which will be null
+        while (temp != null) { //This will go until tail.next if reached which will be null
+            //temp will getPlayer info and then createClone() and add to clone PQList
             clone.add(new Player(temp.getPlayer().createClone()));
+            //Changes to next item in linked list
             temp = temp.getNext();
         }
 
         return clone;
-}
+    }
 
+    /**
+     * Add
+     * Method adds new player to end of PQList
+     * @param a
+     */
     @Override
     public void add(Player a) {
         Node temp = new Node(a);
         //If linkedlist is Empty then it will make head and tail the new temp node
-        if(isEmpty()){
-        head = tail = temp;
-         }else{
+        if (isEmpty()) {
+            head = tail = temp;
+        } else {
             //This will change tail next reference from null to the new node
-        tail.setNext(temp);
-        //This will then change tail from previous node to the new one as it's the new tail
-        tail = temp;
+            tail.setNext(temp);
+            //This will then change tail from previous node to the new one as it's the new tail
+            tail = temp;
         }
         size++;
     }
 
+    /**
+     * getHighestScorePlayer
+     * method will start with head as highest node use as a start to traverse list
+     * if highest is head it will remove node and shift head forward 1
+     * if tail it will set tail to previous node
+     * if in another place it will remove and connect previous node to next after highest
+     * @return
+     */
     @Override
     public Player getHighestScoreplayer() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        Node highest = this.head;//Make head highest since it is start
+        Node prev = null; //keep track of prev node during traversal
+        Node current = this.head;//This will traverse list
+        Node highPrev = null;//previous node of highest
+
+        while (current != null) {
+
+            if (current.getPlayer().getScore() > highest.getPlayer().getScore()) {
+                highest = current;//Set the new highest node
+                highPrev = current;//Keeping track of prev node before highest
+            }
+            prev = current; //move prev forward
+            current = current.getNext();//move current forward
+        }
+        //if highest is head move head forward to next node which will remove it
+        if(highest == this.head) {
+            head = head.getNext();
+        } else if (highest == tail) {//if highest is tail it will set tail to previous node
+            tail = highPrev;
+            tail.setNext(null);
+        }else {//if not tail or h
+        highPrev.setNext(highest.getNext());
+        }
+        size--;
+        return highest.getPlayer();
+
     }
 
+    /**
+     * clear
+     * Method clears List of all data
+     */
     @Override
     public void clear() {
         head = tail = null;
@@ -67,13 +123,41 @@ public PQList createClone(){
 
     }
 
+    /**
+     * getSize
+     * Method method will return current number of nodes in list
+     * @return
+     */
     @Override
     public int getSize() {
         return size;
     }
 
+    /**
+     * isEmpty()
+     * Method checks if there are any nodes in list by seeing if head is null
+     * @return true if list is empty and false if it contains any nodes
+     */
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return head == null;
+    }
+
+    /**
+     * tostring
+     * Returns a string list of PQList
+     * @return String
+     */
+    public String toString() {
+        if (isEmpty()) {
+            return "List is empty";
+        }
+        StringBuilder builder = new StringBuilder();
+        Node current = head;
+        while(current != null) {
+            builder.append(current.getPlayer()).append('\n');
+            current = current.getNext();
+        }
+        return builder.toString();
     }
 }
